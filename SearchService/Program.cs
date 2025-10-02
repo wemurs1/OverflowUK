8df76e63a1bfdd1829d6668d9a4231205b47ba81
Stuart Murray <stuart.murray.dev@outlook.com>
2025-10-02 17:42:28 +0100

    End of section 4

---
+++ SearchService/Program.cs
@@ -0,0 +1,112 @@
+using System.Text.RegularExpressions;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using SearchService.Data;
+using SearchService.Models;
+using Typesense;
+using Typesense.Setup;
+using Wolverine;
+using Wolverine.RabbitMQ;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddOpenApi();
+builder.AddServiceDefaults();
+
+var typesenseUri = builder.Configuration["services:typesense:typesense:0"];
+if (string.IsNullOrEmpty(typesenseUri)) throw new InvalidOperationException("Typesense URI not found in config");
+
+var typesenseApiKey = builder.Configuration["typesense-api-key"];
+if (string.IsNullOrEmpty(typesenseApiKey)) throw new InvalidOperationException("Typesense API key not found in config");
+
+var uri = new Uri(typesenseUri);
+
+builder.Services.AddTypesenseClient(config =>
+{
+    config.ApiKey = typesenseApiKey;
+    config.Nodes = new List<Node>
+    {
+        new(uri.Host, uri.Port.ToString(), uri.Scheme)
+    };
+});
+
+builder.Services.AddOpenTelemetry().WithTracing(traceProviderBuilder =>
+{
+    traceProviderBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService(builder.Environment.ApplicationName))
+        .AddSource("Wolverine");
+});
+
+builder.Host.UseWolverine(opts =>
+{
+    opts.UseRabbitMqUsingNamedConnection("messaging").AutoProvision();
+    opts.ListenToRabbitQueue("questions.search", cfg => { cfg.BindExchange("questions"); });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapDefaultEndpoints();
+
+app.MapGet("/search", async (string query, ITypesenseClient client) =>
+{
+    // [aspire]something
+    string? tag = null;
+
+    var tagMatch = Regex.Match(query, @"\[(.*?)\]");
+    if (tagMatch.Success)
+    {
+        tag = tagMatch.Groups[1].Value;
+        query = query.Replace(tagMatch.Value, "").Trim();
+    }
+
+    var searchParams = new SearchParameters(query, "title,content");
+
+    if (!string.IsNullOrWhiteSpace(tag))
+    {
+        searchParams.FilterBy = $"tags:=[{tag}]";
+    }
+
+    try
+    {
+        var result = await client.Search<SearchQuestion>("questions", searchParams);
+        return Results.Ok(result.Hits.Select(hit => hit.Document));
+    }
+    catch (Exception e)
+    {
+        return Results.Problem("Typesense search failed", e.Message);
+    }
+});
+
+app.MapGet("/search/similar-titles", async (string query, ITypesenseClient client) =>
+{
+    var searchParams = new SearchParameters(query, "title");
+
+    try
+    {
+        var result = await client.Search<SearchQuestion>("questions", searchParams);
+        return Results.Ok(result.Hits.Select(hit => hit.Document));
+    }
+    catch (Exception e)
+    {
+        return Results.Problem("Typesense search failed", e.Message);
+    }
+});
+
+using var scope = app.Services.CreateScope();
+var client = scope.ServiceProvider.GetRequiredService<ITypesenseClient>();
+await SearchInitialiser.EnsureIndexExists(client);
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
